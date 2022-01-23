13e6a6fb59cf7035caf2f7b74a9248efdd0db498
Naruki-Ichihara <70839257+Naruki-Ichihara@users.noreply.github.com>
2022-01-23 16:27:19 +0900

    add 'method' key to the with_minmax_derivative

--- fenics_optimize/__init__.py
+++ fenics_optimize/__init__.py
@@ -4,9 +4,9 @@
 .. include:: ../README.md
 """
 
-__version__ = "0.1.0.alpha"
+__version__ = "0.1.1.alpha"
 
-from .Core import with_derivative, without_derivative, max_derivative_approximation
+from .Core import with_derivative, without_derivative, with_minmax_derivative
 from .Utilities import Recorder, Logger
 from .Solvers import AMGsolver
 from .Optimizer import HSLoptimize, MMAoptimize
