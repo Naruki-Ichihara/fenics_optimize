2521934976ef581f825e81dfef039aa10065647d
Naruki Ichihara <70839257+Naruki-Ichihara@users.noreply.github.com>
2023-03-13 18:10:47 +0900

    Update __init__.py

--- optfx/__init__.py
+++ optfx/__init__.py
@@ -7,5 +7,5 @@ from dolfin import *
 from dolfin_adjoint import *
 from .core import Module
 from .utils import to_numpy, from_numpy
-from .filters import helmholtzFilter, hevisideFilter, box2circleConstraint
+from .filters import helmholtzFilter, hevisideFilter, box2circleConstraint, circle2boxConstraint
 from .optimizer import optimize
